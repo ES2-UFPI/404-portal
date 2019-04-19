55f67ef1bbc560afb0e3506d1f10ceda00838733
felipebarroscaminha <felipebarroscaminha@gmail.com>
2019-04-19 15:48:29 -0300

    create app Noticias

---
+++ backend/noticias/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils import timezone
+
+class Noticia(models.Model):
+    titulo = models.CharField(max_length = 50)
+    subtitulo = models.TextField()
+    conteudo = models.TextField()
+    data_criacao = models.DateTimeField(default = timezone.now)
+    data_publicacao = models.DateTimeField(null = True, blank = True)
+
+    def __str__(self):
+        return self.titulo
--- portal/settings.py
+++ portal/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend.core',
+    'backend.noticias',
 ]
 
 MIDDLEWARE = [
